17c54f83964724e4e3e73b1522f96949020a781e
mmarcec007 <mark.marcec@gmail.com>
2022-01-28 09:14:01 +0100

    enhance the overall example template plugin

--- src/MyNamespace/Pckg/MyExample/MyExampleElement.php
+++ src/MyNamespace/Pckg/MyExample/MyExampleElement.php
@@ -2,11 +2,54 @@
 
 namespace MyNamespace\Pckg\MyExample;
 
-class MyExampleElement extends \Convo\Core\Workflow\AbstractWorkflowComponent implements \Convo\Core\Workflow\IConversationElement
+use Convo\Core\Params\IServiceParamsScope;
+use Convo\Core\Util\ArrayUtil;
+use Convo\Core\Workflow\IConversationElement;
+
+class MyExampleElement extends \Convo\Core\Workflow\AbstractWorkflowContainerComponent implements \Convo\Core\Workflow\IConversationElement
 {
+	/**
+	 * @var string
+	 */
+	private $_direction;
+
+	/**
+	 * @var string
+	 */
+	private $_statusVar;
+
+	/**
+	 * @var array
+	 */
+	private $_directionOptions;
+
+	/**
+	 * @var IConversationElement[]
+	 */
+	private $_goFlow = array();
+
+	/**
+	 * @var IConversationElement[]
+	 */
+	private $_canNotGoFlow = array();
+
 	public function __construct( $properties)
 	{
 		parent::__construct( $properties);
+
+		$this->_direction = $properties['direction'];
+		$this->_statusVar = $properties['status_var'];
+		$this->_directionOptions = $properties['direction_options'];
+
+		foreach ( $properties['go'] as $element) {
+			$this->_goFlow[] = $element;
+			$this->addChild($element);
+		}
+
+		foreach ( $properties['can_not_go'] as $element) {
+			$this->_canNotGoFlow[] = $element;
+			$this->addChild($element);
+		}
 	}
 
 	/**
@@ -15,6 +58,69 @@ class MyExampleElement extends \Convo\Core\Workflow\AbstractWorkflowComponent im
 	 */
 	public function read(\Convo\Core\Workflow\IConvoRequest $request, \Convo\Core\Workflow\IConvoResponse $response)
 	{
-		// TODO: Implement read() method.
+		$direction = $this->evaluateString($this->_direction);
+		$statusVar = $this->evaluateString($this->_statusVar);
+		$directionOptions = $this->_evaluateArgs( $this->_directionOptions);
+		$directionOptionsValues = array_values($directionOptions);
+
+		$params = $this->getService()->getComponentParams( IServiceParamsScope::SCOPE_TYPE_SESSION, $this);
+		$index = $params->getServiceParam($statusVar)['current_index'] ?? 0;
+		$current_direction_options = $params->getServiceParam($statusVar)['current_direction_options'] ?? $directionOptionsValues[$index];
+
+		if ($params->getServiceParam($statusVar)['has_started'] && in_array(strtolower($direction), $current_direction_options)) {
+			if (strtolower($direction) === 'forward') {
+				$index++;
+				if ($index >= count($directionOptionsValues)) {
+					$index = count($directionOptionsValues) - 1;
+				}
+			} else if (strtolower($direction) === 'backward') {
+				$index--;
+				if ($index < 0) {
+					$index = 0;
+				}
+			}
+		}
+
+		$params->setServiceParam($statusVar, [
+			'has_started' => true,
+			'direction_options' => $directionOptionsValues,
+			'current_direction_options' => $directionOptionsValues[$index],
+			'current_index' => $index,
+			'last' => $index === count($directionOptionsValues) - 1
+		]);
+
+		if (in_array(strtolower($direction), $current_direction_options)) {
+			$selected_flow = $this->_goFlow;
+		} else {
+			$selected_flow = $this->_canNotGoFlow;
+		}
+
+		foreach ($selected_flow as $element) {
+			$element->read($request, $response);
+		}
+	}
+
+	private function _evaluateArgs($args)
+	{
+		// $this->_logger->debug( 'Got raw args ['.print_r( $args, true).']');
+		$returnedArgs = [];
+		foreach ($args as $key => $val)
+		{
+			$key	=	$this->evaluateString($key);
+			$parsed =   $this->evaluateString($val);
+
+			if (!ArrayUtil::isComplexKey($key))
+			{
+				$returnedArgs[$key] = $parsed;
+			}
+			else
+			{
+				$root = ArrayUtil::getRootOfKey($key);
+				$final = ArrayUtil::setDeepObject($key, $parsed, $returnedArgs[$root] ?? []);
+				$returnedArgs[$root] = $final;
+			}
+		}
+		// $this->_logger->debug( 'Got evaluated args ['.print_r( $returnedArgs, true).']');
+		return $returnedArgs;
 	}
 }
--- src/MyNamespace/Pckg/MyExample/MyExamplePackageDefinition.php
+++ src/MyNamespace/Pckg/MyExample/MyExamplePackageDefinition.php
@@ -5,7 +5,7 @@ namespace MyNamespace\Pckg\MyExample;
 use Convo\Core\Factory\AbstractPackageDefinition;
 use Convo\Core\Intent\EntityModel;
 use Convo\Core\Intent\SystemEntity;
-use Convo\Core\Eval\ExpressionFunction;
+use Convo\Core\Expression\ExpressionFunction;
 
 class MyExamplePackageDefinition extends AbstractPackageDefinition
 {
@@ -22,18 +22,27 @@ class MyExamplePackageDefinition extends AbstractPackageDefinition
 	protected function _initEntities()
 	{
 		$entities = [];
-		$entities['EntityName'] = new SystemEntity('EntityName');
-		$entity_name_model = new EntityModel('EntityName', false);
+		$entities['Direction'] = new SystemEntity('Direction');
+		$entity_name_model = new EntityModel('Direction', false);
 		$entity_name_model->load([
-			"name" => "EntityName",
+			"name" => "Direction",
 			"values" => [
 				[
-					"value" => "Entity 1",
+					"value" => "left",
+				],
+				[
+					"value" => "right",
+				],
+				[
+					"value" => "forward",
+				],
+				[
+					"value" => "backward",
 				]
 			]
 		]);
-		$entities['EntityName']->setPlatformModel('amazon', $entity_name_model);
-		$entities['EntityName']->setPlatformModel('dialogflow', $entity_name_model);
+		$entities['Direction']->setPlatformModel('amazon', $entity_name_model);
+		$entities['Direction']->setPlatformModel('dialogflow', $entity_name_model);
 
 		return $entities;
 	}
@@ -71,20 +80,73 @@ class MyExamplePackageDefinition extends AbstractPackageDefinition
 				'\MyNamespace\Pckg\MyExample\MyExampleElement',
 				'My Example Element',
 				'Change Me.',
-				array(
-					'_preview_angular' => array(
-						'type' => 'html',
-						'template' => '<div class="code">' .
-							'<span class="statement">My Example Element </span>' .
-							'</div>'
+				[
+					'direction' => [
+						'editor_type' => 'text',
+						'editor_properties' => [],
+						'defaultValue' => null,
+						'name' => 'Direction',
+						'description' => 'Direction where to go.',
+						'valueType' => 'string'
+					],
+					'status_var' => array(
+						'editor_type' => 'text',
+						'editor_properties' => array(),
+						'defaultValue' => 'status',
+						'name' => 'Status variable',
+						'description' => 'Variable name for accessing element status variable information, such as the current index and available directions.',
+						'valueType' => 'string'
 					),
+					'direction_options' => [
+						'editor_type' => 'params',
+						'editor_properties' => [
+							'multiple' => true
+						],
+						'defaultValue' => array(),
+						'name' => 'Direction Options',
+						'description' => 'Set of directions where the text game progresses.',
+						'valueType' => 'array'
+					],
+					'go' => [
+						'editor_type' => 'service_components',
+						'editor_properties' => [
+							'allow_interfaces' => ['\Convo\Core\Workflow\IConversationElement'],
+							'multiple' => true
+						],
+						'defaultValue' => [],
+						'defaultOpen' => false,
+						'name' => 'Going',
+						'description' => 'Flow to be executed if can go in certain direction.',
+						'valueType' => 'class'
+					],
+					'can_not_go' => [
+						'editor_type' => 'service_components',
+						'editor_properties' => [
+							'allow_interfaces' => ['\Convo\Core\Workflow\IConversationElement'],
+							'multiple' => true
+						],
+						'defaultValue' => [],
+						'defaultOpen' => false,
+						'name' => 'Can not go',
+						'description' => 'Flow to be executed if can not go in certain direction.',
+						'valueType' => 'class'
+					],
+					'_preview_angular' => [
+						'type' => 'html',
+						'template' => '<div class="code">Example Element <br>' .
+							' <span ng-if="!component.properties[\'_use_var_properties\']" ng-repeat="(key, val) in component.properties.direction_options track by key">' .
+							' <b>{{ key}}</b> = <b>{{ val }};</b><br>' .
+							' </span>' .
+							'<span ng-if="component.properties[\'_use_var_properties\']">{{ component.properties.properties }}</span>' .
+							'</div>'
+					],
 					'_interface' => '\Convo\Core\Workflow\IServiceContext',
 					'_workflow' => 'read',
-					'_help' =>  array(
+					'_help' =>  [
 						'type' => 'file',
 						'filename' => 'my-example-element.html'
-					),
-				)
+					],
+				]
 			)
 		);
 	}
